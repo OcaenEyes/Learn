849ac212de38210a129835cddc0baa437b85fdc8
OcaenEyes <1450136519@qq.com>
2018-06-02 00:05:09 +0800

    可输出中文
    输出编码

--- pythonLearn/python_Test01.py
+++ pythonLearn/python_Test01.py
@@ -1,8 +1,18 @@
-#-*- coding:utf-8 -*-
+#!/usr/bin/python3
+#-*- coding:UTF-8 -*-
 import sys
 import locale
+import io
 import os
 import codecs
 print(sys.getdefaultencoding()+" - sys.getdefaultencoding")
 
 print(sys.stdout.encoding + " - sys.stout.encoding")
+
+# 改变标准输出的默认编码
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding = 'utf-8')
+
+a = '测试'
+b = u'测试'
+print (a)
+print (b) 
