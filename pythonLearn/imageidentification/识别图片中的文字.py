19ce49c35da443427329550be412e0738268e552
OcaenEyes <1450136519@qq.com>
2018-06-18 14:14:34 +0800

    updtate
    update

--- pythonLearn/imageidentification/识别图片中的文字.py
+++ pythonLearn/imageidentification/识别图片中的文字.py
@@ -11,6 +11,6 @@ import time
 time1 = time.time()
 print(time1)
 
-image = Image.open('/Users/gaozhiyong/Documents/GitHub/python/Learn/pythonLearn/图像识别/zimu.jpeg')
+image = Image.open('/Users/gaozhiyong/Documents/GitHub/python/Learn/pythonLearn/imageidentification/zimu.png')
 content_text=pytesseract.image_to_string(image)
 print(content_text)
